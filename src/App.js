63760f3ec8e1d30b846035151ce09304817e64ce
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-08-30 04:02:07 +0000

    created lobby component for player

--- src/App.js
+++ src/App.js
@@ -5,11 +5,28 @@ import io from 'socket.io-client';
 import axios from 'axios';
 import Answer from "./components/Answer";
 import Question from "./components/Question";
+import PlayerLobby from "./components/player_lobby/PlayerLobby";
 
 // const base = io('/');
 
 const fQuestions = "what is 1+1?";
 const fanswers = [{content: "2", correct: true}, {content: "3", correct: false}, {content:"4", correct: false}];
+
+const playerData = [
+  {user_id: 1, player_gamertag: "Hero", score: "", is_host: false},
+  {user_id: 2, player_gamertag: "Paws", score: "", is_host: false},
+  {user_id: 3, player_gamertag: "Kylo", score: "", is_host: true},
+  {user_id: 4, player_gamertag: "Loko", score: "", is_host: false}
+];
+
+const users = {
+  1:{id: 1, name: "Henry", email: "henry@xp.com", password: "###", user_expertise_level: "S Rank"},
+  2:{id: 2, name: "Felipe", email: "felipe@xp.com", password: "###", user_expertise_level: "B Rank"},
+  3:{id: 3, name: "Gurcharan", email: "chanchan@xp.com", password: "###", user_expertise_level: "A Rank"},
+  4:{id: 4, name: "Rob", email: "elite@xp.com", password: "###", user_expertise_level: "C Rank"}
+};
+
+
 
 export default function App() {
   const socket = io('http://localhost:8080');
@@ -29,13 +46,13 @@ export default function App() {
           console.log(message + " and " + socket.id);
         }}
         >
-          TEST
+          Test
         </button>
-        {/* <Question
-          key={1}
-          question={fQuestions}
-          answers={fanswers}
-        /> */}
+        <h3>Lobby</h3>
+        <PlayerLobby
+          // player={playerData}
+          // users={users}
+        />
       </header>
     </div>
   )
