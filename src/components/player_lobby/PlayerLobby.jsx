07903d09654c9974abe550086c56dd5e20c51d28
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-08-31 20:48:24 +0000

    create player lobby component that renders the players

--- src/components/player_lobby/PlayerLobby.jsx
+++ src/components/player_lobby/PlayerLobby.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import PlayerLobbyList from './PlayerLobbyList';
+// import {ValueContext} from "../../App.js";
 
 export default function Lobby(props) {
-  let players = props.player.map((player, index) => {
-    return <PlayerLobbyList
-        gamertag = {player.player_gamertag}
-        host = {player.is_host}
-        level = {props.users[player.user_id].user_expertise_level}
-      />
-  })
+
+  // const {value} = React.useContext(ValueContext);
+
+  // const [players, setPlayers] = React.useState([]);
+
+  // React.useEffect(() => {
+  //   console.log("playersRound1:", players);
+  //   setPlayers(props.player);
+  //   console.log("playersRound2:", players);
+  //   // createPlayerList();
+  // }, [players, props.player]);
+
+
+    const playersData = props.players.map((player, index) => {
+      return <PlayerLobbyList
+                key={player.user_id}
+                gamertag = {player.player_gamertag}
+                host = {player.is_host}
+                level = {props.users[player.user_id].user_expertise_level}
+              />
+    })
+
   return (
     <section>
       <div>
-        <h4>{players}</h4>
+        <h4>{playersData}</h4>
       </div>
     </section>
   )
