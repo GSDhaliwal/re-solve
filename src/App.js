f43e5b3ea5047e12670d2769c47f15cc0ec005a5
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-09-03 01:03:49 +0000

    create websocket for game onlick

--- src/App.js
+++ src/App.js
@@ -6,29 +6,8 @@ import axios from 'axios';
 import Answer from "./components/Answer";
 import Question from "./components/Question";
 import GamesList from "./components/host_games_list/GamesList";
-import UserContext from "./components/UserContext";
+import createContext from "./components/createContext";
 
-// const base = io('/');
-
-// const fQuestions = "what is 1+1?";
-// const fanswers = [{content: "2", correct: true}, {content: "3", correct: false}, {content:"4", correct: false}];
-
-// const quizzes = [
-//   {id: 1, category_id: 1, user_id: 1, quiz_name: "Middle Earth", num_of_questions: 5, difficulty: 9.8, rating: 7, num_of_times_hosted: 10, total_players_played: 200},
-//   {id: 2, category_id: 1, user_id: 2, quiz_name: "Star Wars", num_of_questions: 5, difficulty: 5.5, rating: 7, num_of_times_hosted: 10, total_players_played: 200},
-//   {id: 3, category_id: 2, user_id: 3, quiz_name: "Harry Potter", num_of_questions: 5, difficulty: 1.5, rating: 7, num_of_times_hosted: 10, total_players_played: 200},
-//   {id: 4, category_id: 1, user_id: 4, quiz_name: "Star Trek", num_of_questions: 5, difficulty: 4, rating: 7, num_of_times_hosted: 10, total_players_played: 200},
-//   {id: 5, category_id: 2, user_id: 5, quiz_name: "Azeroth", num_of_questions: 5, difficulty: 8, rating: 7, num_of_times_hosted: 10, total_players_played: 200},
-//   {id: 6, category_id: 1, user_id: 6, quiz_name: "Lego Universe", num_of_questions: 5, difficulty: 7.5, rating: 7, num_of_times_hosted: 10, total_players_played: 200},
-//   {id: 7, category_id: 3, user_id: 7, quiz_name: "Castle Peach", num_of_questions: 5, difficulty: 4.4, rating: 7, num_of_times_hosted: 10, total_players_played: 200},
-//   {id: 8, category_id: 3, user_id: 8, quiz_name: "Rob's Appartment", num_of_questions: 5, difficulty: 7.2, rating: 7, num_of_times_hosted: 10, total_players_played: 200}
-// ];
-
-// const categories = [
-//   {id:1, category_name: "Math"},
-//   {id:2, category_name: "History"},
-//   {id:3, category_name: "Code"}
-// ];
 
 export default function App() {
 
@@ -39,27 +18,16 @@ export default function App() {
   const [initilizedCategory, setInitializedCategory] = useState(false);
 
   const socket = io('http://localhost:8080');
-  
   let message;
-  // socket.on('gameslist', (data=>{
-  //   setState(data);
-  // }));
 
-  
+  const createGame = (quizID)=>{
+    socket.emit('hostableGame', {quizID});
+  };
 
   socket.on('gameslist', (data=>{
     setQuiz(data);
     setInitializedQuiz(true);
   }));
-
-  // socket.on('gameslistQuizzes', (data=>{
-  //   setQuiz(data);
-  //   setInitializedQuiz(true);
-  // }));
-  // socket.on('gameslistCategories', (data=>{
-  //   setCategory(data);
-  //   setInitializedCategory(true);
-  // }));
   
   useEffect(() =>{
     console.log("logging quiz: ", quiz);
@@ -98,13 +66,11 @@ export default function App() {
         >
           TEST
         </button>
-        {/* <UserContext.Provider value = {{state, user}}> */}
+        <createContext.Provider value = {{createGame}}>
           <GamesList
-            // quizzes={state}
             quizzes={quiz}
-            // categories={category}
           />
-        {/* </UserContext.Provider> */}
+        </createContext.Provider>
       </header>
     </div>
   )
