177190261562f4276e5ef71fa721a10fbe4f93d6
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-09-07 19:45:08 +0000

    added new back buttons and nav

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,26 @@
 import React, { useContext, useEffect } from 'react';
 import createContext from '../createContext';
 import Login from "../Login"
+import {
+  BrowserRouter as Router,
+  useHistory
+} from "react-router-dom";
 
 
 export default function Profile (props){
 
+  //back button functionality
+  let history = useHistory();
+  function handleClick() {
+    history.push("/");
+  }
+
   return (
     <section>
       <h4>Profile Page</h4>
+      <button type = "button" onClick={()=>{
+        handleClick()
+      }}>Back</button>
       <Login/>
     </section>
   )
