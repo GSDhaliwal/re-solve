ad8da017e74a6926091d21084a5ced5569526c81
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-09-07 19:44:51 +0000

    added new back buttons and nav

--- src/components/ManageAccount/ManageAccount.jsx
+++ src/components/ManageAccount/ManageAccount.jsx
@@ -1,11 +1,21 @@
 import React, { useContext } from 'react';
 import createContext from '../createContext';
 import UserQuizzes from './UserQuizzes'
+import {
+  BrowserRouter as Router,
+  useHistory
+} from "react-router-dom";
 
 
 export default function ManageAccount(props) {
 
   const context = useContext(createContext);
+
+  //back button functionality
+  let history = useHistory();
+  function handleClick() {
+    history.push("/");
+  }
   
   
 
@@ -26,6 +36,9 @@ export default function ManageAccount(props) {
   return (
     <section>
       <h1>Hey</h1>
+      <button type = "button" onClick={()=>{
+        handleClick()
+      }}>Back</button>
       {userQuizzes}
     </section>
   )
