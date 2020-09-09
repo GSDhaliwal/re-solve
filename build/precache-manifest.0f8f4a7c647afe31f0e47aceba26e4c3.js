714907ea8d35474da076ed0278afd035037a737f
HenryMao <henry60308@live.cn>
2020-09-09 09:32:52 +0000

    build

---
+++ build/precache-manifest.0f8f4a7c647afe31f0e47aceba26e4c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db3d7901ee26be6a1eaba75d3e7eb68b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56329011f9327cda059c",
+    "url": "/static/css/main.ad0b051f.chunk.css"
+  },
+  {
+    "revision": "b023d8b7234745610a0a",
+    "url": "/static/js/2.27dc1b48.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.27dc1b48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56329011f9327cda059c",
+    "url": "/static/js/main.b1a4f7c0.chunk.js"
+  },
+  {
+    "revision": "6beabcfb79496c8f81c9",
+    "url": "/static/js/runtime-main.bb556c7b.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://re-solve.herokuapp.com/');
 
 ReactDOM.render(
   
